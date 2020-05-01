de0703ae85355792f4a6ece737f8995f4ebb0e00
felipesarian <felipe.sarian@gmail.com>
2020-05-01 16:55:28 -0300

    adicionei botao e label

--- tkinter_example.py
+++ tkinter_example.py
@@ -1,8 +1,12 @@
 from tkinter import *
 
 window = Tk()
-window.geometry('350x200')
+window.geometry('450x400')
 
 window.title("Welcome TKInter")
+lbl = Label(window, text='hello', font=('Arial Bold', 50))
+lbl.grid(column = 0, row = 0)
+btn = Button(window, text='clique em mim')
+btn.grid(column = 1, row = 1)
 
 window.mainloop()
