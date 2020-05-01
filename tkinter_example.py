ac54fc35ed11029ea12c52ce1f1bb1a55814ef23
felipesarian <felipe.sarian@gmail.com>
2020-05-01 19:05:57 -0300

    login

--- tkinter_example.py
+++ tkinter_example.py
@@ -1,12 +1,26 @@
-from tkinter import *
+import tkinter as tk
 
-window = Tk()
-window.geometry('450x400')
-
+window = tk.Tk()
+window.geometry('400x400')
 window.title("Welcome TKInter")
-lbl = Label(window, text='hello', font=('Arial Bold', 50))
-lbl.grid(column = 0, row = 0)
-btn = Button(window, text='clique em mim')
-btn.grid(column = 1, row = 1)
+
+titulo = tk.Label(window, text='Log-in', font=('Arial Bold', 50))
+titulo.grid(column = 0, row = 0)
+
+nome = tk.Label(window, text='Nome de usuário:', font=('Arial Bold', 20))
+nome.grid(column = 0, row = 3)  
+
+nomeescrever = tk.Entry(window)
+nomeescrever.grid(column = 0, row = 4)
+
+
+senha = tk.Label(window, text='Senha:', font=('Arial Bold', 20))
+senha.grid(column = 0, row = 6)
+
+senhaescrever = tk.Entry(window)
+senhaescrever.grid(column = 0, row = 7)
+
+enter = tk.Button(window, text='Enter')
+enter.grid(column = 0, row = 9)
 
 window.mainloop()
